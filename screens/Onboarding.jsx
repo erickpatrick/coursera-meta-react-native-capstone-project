017cc187082bd56fd6c057037acdd29d272ca8c6
erick patrick <github@erickpatrick.net>
2025-01-21 16:22:13 +0100

    move content to onboarding.jsx file

---
+++ screens/Onboarding.jsx
@@ -0,0 +1,57 @@
+import { Button, Text, TextInput, View } from "react-native";
+import LittleLemonLogo from "../components/LittleLemonLogo";
+import { useState } from "react";
+
+export default function Onboarding() {
+    const [username, onChangeUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [validEmail, setValidEmail] = useState(false)
+    const isButtonDisabled = username.length === 0 || email.length === 0 || !validEmail
+
+    function validateEmail(email) {
+        const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        return expression.test(String(email).toLowerCase())
+    }
+
+    function onChangeEmail(email) {
+        if (validateEmail(email)) {
+            setValidEmail(true)
+        } else {
+            setValidEmail(false)
+        }
+
+        setEmail(email)
+    }
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                alignItems: "center",
+                backgroundColor: 'white',
+                padding: 16
+            }}
+        >
+            <View style={{ height: 50, width: "100%", alignItems: "center" }}>
+                <LittleLemonLogo />
+            </View>
+            <View style={{ width: "100%", alignItems: "flex-start" }}>
+                <Text>Username</Text>
+                <TextInput
+                    style={{ borderColor: "lightgray", borderWidth: 1, width: "100%", marginBottom: 16, padding: 16 }}
+                    onChangeText={onChangeUsername}
+                    value={username}
+                />
+                <Text>E-mail</Text>
+                <TextInput
+                    style={{ borderColor: "lightgray", borderWidth: 1, width: "100%", marginBottom: 16, padding: 16 }}
+                    onChangeText={onChangeEmail}
+                    value={email}
+                />
+            </View>
+            <View style={{ height: 50, width: "100%", alignItems: "flex-end" }}>
+                <Button title="Next" style={{ backgroundColor: "green" }} disabled={isButtonDisabled}>Next</Button>
+            </View>
+        </View>
+    );
+}
