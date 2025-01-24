46032d420e7d1bf8e61a979a782c75bf2af7ce04
erick patrick <github@erickpatrick.net>
2025-01-25 00:42:45 +0100

    adds proper alert from react native

--- app/profile.jsx
+++ app/profile.jsx
@@ -9,7 +9,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as ImagePicker from 'expo-image-picker';
 import { useNavigation, useRouter } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { Image, ScrollView, Text, View } from 'react-native';
+import { Alert, Image, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Profile() {
@@ -58,7 +58,7 @@ export default function Profile() {
       await AsyncStorage.setItem("@ProfileNotificationNewsletter", newsletter)
       await AsyncStorage.setItem("@ProfilePicture", image)
     })()
-    alert('saved')
+    Alert.alert("Changes saved", "The changes made were properly saved")
   }
 
   async function retrieveAllFromAsyncStorage() {
